efd45acd1c0a6eafcf4e7ea1fc06b0beac8bda99
deadcube04 <gabrielmca2004@gmail.com>
2025-04-07 18:34:25 -0300

    starting Spring

---
+++ ordem-back/OP/src/test/java/edu/cesar/taverna/bd/OP/OpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cesar.taverna.bd.OP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
