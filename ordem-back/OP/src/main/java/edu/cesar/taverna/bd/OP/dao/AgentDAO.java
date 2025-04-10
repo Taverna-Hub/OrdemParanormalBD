a1700a9f8fa453179dd2fd4fa4ea714484c8a8ef
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2025-04-10 10:10:05 -0300

    minor changes

--- ordem-back/OP/src/main/java/edu/cesar/taverna/bd/OP/dao/AgentDAO.java
+++ ordem-back/OP/src/main/java/edu/cesar/taverna/bd/OP/dao/AgentDAO.java
@@ -14,23 +14,20 @@ public class AgentDAO {
 //        System.out.println(agent.getLogin());
 
         String sql = "INSERT INTO agents (id_agent, name, birth_date, tel_number, andress, prestige, nex, retired, trancended)" +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (Connection conn = ConnectionFactory.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, agent.getId().toString());
             stmt.setString(2, agent.getName());
-//            stmt.setString(3, agent.getLogin());
             stmt.setDate(3, java.sql.Date.valueOf(agent.getBirthDate()));
-            // certifique-se de que o driver JDBC lida com LocalDate
             stmt.setString(4, agent.getTelNumber());
             stmt.setString(5, agent.getAndress());
             stmt.setString(6, agent.getPrestige());
             stmt.setInt(7, agent.getNex());
             stmt.setBoolean(8, agent.isRetired());
             stmt.setBoolean(9, agent.isTrancended());
-//            stmt.setString(11, agent.getPassword());
 
             stmt.executeUpdate();
 
