7eeb0d7f8eca68eabdd7cc51fd5bbf73e3073ac0
Sophia-15 <deletrasopa@gmail.com>
2025-04-22 00:35:12 -0300

    configuring .evn

--- ordem-back/OP/src/main/java/edu/cesar/taverna/bd/OP/config/ConnectionFactory.java
+++ ordem-back/OP/src/main/java/edu/cesar/taverna/bd/OP/config/ConnectionFactory.java
@@ -1,17 +1,25 @@
 package edu.cesar.taverna.bd.OP.config;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionFactory {
-    private static final Dotenv dotenv =
-            Dotenv.configure().directory("./").load();
+    private static final Dotenv dotenv;
+
+    static {
+        File envFile = new File(".env");
+
+        if (envFile.exists()) {
+            dotenv = Dotenv.configure().directory("./").load();
+        } else {
+            dotenv = Dotenv.configure().directory("./ordem-back/OP").load();
+        }
+    }
 
     public static Connection getConnection() throws SQLException{
-
-
         return DriverManager.getConnection(
                 dotenv.get("DB_URL"),
                 dotenv.get("DB_USER"),
